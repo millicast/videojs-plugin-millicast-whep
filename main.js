52e63a9867ec131ca4016fa2e5dbcacacacca1d2
Nicolás Inzua Herken <nicolasinzua@hotmail.com>
2023-03-24 17:18:08 -0300

    WHEP JS plugin with layers selection

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 import videojs from 'video.js'
 import './videojs-millicast-viewer'
 import MillicastWhepPlugin from './videojs-millicast-viewer';
+import 'videojs-resolution-switcher-webpack/lib/videojs-resolution-switcher.css'
+
+const url = 'https://director-dev.millicast.com/api/whep/CacWx8/s'
 
 videojs.registerPlugin('MillicastWhepPlugin', MillicastWhepPlugin)
 
 // Configure Video.js options
-var options = { muted: true };
+var options = { 
+  muted: true
+};
 
 // Initialize Video.js player
-videojs('my-video', options, function onPlayerReady() {
+let player = videojs('my-video', options, function onPlayerReady() {
   videojs.log('Your player is ready!');
-  this.MillicastWhepPlugin({ url: "https://director.millicast.com/api/whep/QYrKNJ/ldunu74i" })
+  this.MillicastWhepPlugin({ url })
 });
+
+player.fluid(true);
--- videojs-millicast-viewer.js
+++ videojs-millicast-viewer.js
@@ -1,19 +1,30 @@
 import videojs from 'video.js'
 import { WHEPClient } from 'whip/whep'
+import 'videojs-resolution-switcher-webpack'
 
 const Plugin = videojs.getPlugin('plugin')
 const ModalDialog = videojs.getComponent('ModalDialog')
 
+// const videojsres = videojs.getComponent('videoJsResolutionSwitcher')
 
 export default class MillicastWhepPlugin extends Plugin {
     constructor(player, options) {
         super(player, options);
+
+        // Work around to avoid using src method instead of srcObject  
+        player.src = () => null
+
+        this.url = options.url
 
         this.modal = new ModalDialog(player, {
             temporary: false,
             label: 'Offline',
             uncloseable: true
         })
+        
+        this.isLayersLoaded = false
+        this.layersAvailable = [] 
+
         player.addChild(this.modal)
 
         this.vid = player.tech().el();
@@ -26,8 +37,6 @@ export default class MillicastWhepPlugin extends Plugin {
 
         this.stream = new MediaStream();
         this.vid.srcObject = this.stream;
-
-        // Initialize empty media stream object
 
         // Create Peer Connection
         this.pc = new RTCPeerConnection();
@@ -43,19 +52,40 @@ export default class MillicastWhepPlugin extends Plugin {
         videojs.log('Before WHEP connection')
         //Start publishing
         this.millicastView(player, options)
+
+        player.videoJsResolutionSwitcher( {
+            ui: true,
+            customSourcePicker: (p) => { return p },
+            dynamicLabel: false
+        });
+        
+        player.on('resolutionchange', () => {
+          const encodingId = player.currentResolution().sources[0].res;
+          this.selectLayer(encodingId);
+        });
     }
 
     millicastView = async (player, options) => {
         //Create whip client
-        var whep = new WHEPClient();
+        this.whep = new WHEPClient();
         try {
-            const whepResponse = await whep.view(this.pc, options.url);
+            const whepResponse = await this.whep.view(this.pc, options.url);
             this.modal.close()
             // Add tracks transceiver receiver tracks to our Media Stream object
             this.pc.getReceivers().forEach((r) => {
                 this.stream.addTrack(r.track)
             })
             player.play();
+            // Listen for whep events
+            await this.waitForEventSource().then( eventSource => {
+                eventSource.addEventListener('layers', (event) => {
+                    this.layersAvailable = JSON.parse(event.data).medias[0].layers
+                    if (!this.isLayersLoaded) {
+                        this.initQualityMenu(event)
+                        this.isLayersLoaded = true    
+                    }    
+                })
+            })
         }
          catch (error) {
             const modalContent = document.createElement('h2')
@@ -70,6 +100,39 @@ export default class MillicastWhepPlugin extends Plugin {
                 this.millicastView(player, options)
             }, 2000);
         }
+    }
+
+    initQualityMenu(event) {
+        const data = JSON.parse(event.data)
+        const layers = data['medias'][0].layers
+        const sources = layers.map(layer => ({
+            src: this.url,
+            type: 'video/mp4',
+            label: layer.encodingId === '2' ? 'High' : layer.encodingId === '1' ? 'Medium' : 'Low',
+            res: layer.encodingId,
+        }));
+        this.player.updateSrc(sources);
+    }
+
+    selectLayer = async (encodingId) => {
+        const layerSelected = this.layersAvailable.filter(l => l.encodingId === encodingId)
+        await this.whep.unselectLayer()
+        await this.whep.selectLayer(layerSelected[0]);
+    }
+
+    waitForEventSource = async () => {
+        return new Promise((resolve) => {
+            if ( this.whep.eventSource ) {
+                resolve(this.whep.eventSource)
+            } else {
+                const checkEventSource = setInterval(() => {
+                        if ( this.whep.eventSource ) {
+                            clearInterval(checkEventSource)
+                            resolve(this.whep.eventSource)
+                        }   
+                }, 100)
+            }    
+        })
     }
 }
 
